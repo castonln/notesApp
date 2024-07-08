955c73b434ac19d786b45bab011b66b8f9a094c6
logan <rubixdatcubix@gmail.com>
2024-07-08 16:09:26 -0400

    add notes from frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,19 @@ import { Button, Col, Container, Row } from 'react-bootstrap';
 import { Note as NoteModel } from './models/note';
 import Note from './components/Note';
 import styles from "./styles/NotesPage.module.css";
+import stylesUtils from "./styles/utils.module.css";
+import * as NotesApi from "./network/notes_api";
+import AddNoteDialogue from './components/AddNoteDialogue';
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
 
+  const [showAddNoteDialogue, setShowAddNoteDialogue] = useState(false);
+
   useEffect(() => {
     async function loadNotes() {
       try {
-        const response = await fetch("/api/notes", { method: "GET" });
-        const notes = await response.json();
+        const notes = await NotesApi.fetchNotes();
         setNotes(notes);
       } catch (error) {
         console.error(error);
@@ -25,13 +29,32 @@ function App() {
 
   return (
     <Container>
+
+      <Button 
+      className={`mb-4 ${stylesUtils.blockCenter}`}
+      onClick={() => setShowAddNoteDialogue(true)}>
+        Add new note
+      </Button>
+
       <Row xs={1} md={2} xl={3} className='g-4'>
         {notes.map(note => (
+
           <Col key={note._id}>
             <Note note={note} className={styles.note} />
           </Col>
+
         ))}
       </Row>
+
+      { showAddNoteDialogue && 
+      <AddNoteDialogue 
+      onDismiss={() => setShowAddNoteDialogue(false)}
+      onNoteSaved={(newNote) => {
+        setNotes([...notes, newNote]);
+        setShowAddNoteDialogue(false);
+      }}/> } 
+      {/* If you passed showAddNoteDialogue as a prop, you would maintain state after closing. */}
+
     </Container>
   );
 }
