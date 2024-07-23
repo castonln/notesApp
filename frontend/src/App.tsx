7dd376f914acfe589c97bee96871fdd6960eac42
logan <rubixdatcubix@gmail.com>
2024-07-22 22:46:54 -0400

    page routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,19 @@
+import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginModal from './components/LoginModal';
 import NavBar from './components/NavBar';
 import SignUpModal from './components/SignUpModal';
-import styles from "./styles/NotesPage.module.css";
-import { useEffect, useState } from 'react';
 import { User } from './models/user';
 import * as NotesApi from "./network/notes_api";
-import NotesPageLoggedInView from './components/NotesPageLoggedInView';
-import NotesPageLoggedOutView from './components/NotesPageLoggedOutView';
+import NotesPage from './pages/NotesPage';
+import NotFoundPage from './pages/NotFoundPage';
+import PrivacyPage from './pages/PrivacyPage';
+import styles from "./styles/App.module.css";
 
 function App() {
 
-  const [loggedInUser, setLoggedInUser] = useState<User|null>(null);
+  const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
 
   const [showSignUpModal, setShowSignUpModal] = useState(false);
   const [showLoginModal, setShowLoginModal] = useState(false);
@@ -29,26 +31,35 @@ function App() {
   }, []); // only execute once
 
   return (
-    <div>
-      <NavBar
-        loggedInUser={loggedInUser}
-        onLoginClicked={() => setShowLoginModal(true)}
-        onSignUpClicked={() => setShowSignUpModal(true)}
-        onLogoutSuccessful={() => setLoggedInUser(null)}
-      />
-      <Container className={styles.notesPage}>
-        <>
-        {loggedInUser
-          ? <NotesPageLoggedInView />
-          : <NotesPageLoggedOutView />
-        }
-        </>
-      </Container>
+    <BrowserRouter>
+      <div>
+        <NavBar
+          loggedInUser={loggedInUser}
+          onLoginClicked={() => setShowLoginModal(true)}
+          onSignUpClicked={() => setShowSignUpModal(true)}
+          onLogoutSuccessful={() => setLoggedInUser(null)}
+        />
+        <Container className={styles.pageContainer}>
+          <Routes>
+            <Route
+              path="/"
+              element={<NotesPage loggedInUser={loggedInUser} />}
+            />
+            <Route 
+              path="/privacy"
+              element={<PrivacyPage />}
+            />
+            <Route 
+              path="/*"
+              element={<NotFoundPage />}
+            />
+          </Routes>
+        </Container>
 
-      {showSignUpModal &&
+        {showSignUpModal &&
           <SignUpModal
             onDismiss={() => setShowSignUpModal(false)}
-            onSignUpSuccessful={(user) => { 
+            onSignUpSuccessful={(user) => {
               setLoggedInUser(user);
               setShowSignUpModal(false);
             }}
@@ -58,13 +69,14 @@ function App() {
         {showLoginModal &&
           <LoginModal
             onDismiss={() => setShowLoginModal(false)}
-            onLoginSuccessful={(user) => { 
+            onLoginSuccessful={(user) => {
               setLoggedInUser(user);
               setShowLoginModal(false);
             }}
           />
         }
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,8 +1,8 @@
-import { Container, Nav, Navbar, NavbarCollapse } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { User } from "../models/user";
-import styles from "../styles/NavBar.module.css";
-import NavBarLoggedInView from "./NavBarLogggedInView";
 import NavBarLoggedOutView from "./NavBarLoggedOutView";
+import NavBarLoggedInView from "./NavBarLogggedInView";
 
 interface NavBarProps {
     loggedInUser: User | null,  // could be logged in, could not
@@ -12,24 +12,29 @@ interface NavBarProps {
 }
 
 const NavBar = ({ loggedInUser, onSignUpClicked, onLoginClicked, onLogoutSuccessful }: NavBarProps) => {
-    return ( 
+    return (
         <Navbar bg="primary" variant="dark" expand="sm" sticky="top">
             <Container>
-                <Navbar.Brand>
+                <Navbar.Brand as={Link} to={"/"}>
                     Cool Notes App
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="main-navbar" />
                 <Navbar.Collapse id="main-navbar">
+                    <Nav>
+                        <Nav.Link as={Link} to="/privacy">
+                            Privacy
+                        </Nav.Link>
+                    </Nav>
                     <Nav className="ms-auto">
-                        { loggedInUser
-                        ? <NavBarLoggedInView user={loggedInUser} onLogoutSuccessful={onLogoutSuccessful} />
-                        : <NavBarLoggedOutView onLoginClicked={onLoginClicked} onSignUpClicked={onSignUpClicked} /> // prop drilling
+                        {loggedInUser
+                            ? <NavBarLoggedInView user={loggedInUser} onLogoutSuccessful={onLogoutSuccessful} />
+                            : <NavBarLoggedOutView onLoginClicked={onLoginClicked} onSignUpClicked={onSignUpClicked} /> // prop drilling
                         }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-     );
+    );
 }
- 
+
 export default NavBar;
